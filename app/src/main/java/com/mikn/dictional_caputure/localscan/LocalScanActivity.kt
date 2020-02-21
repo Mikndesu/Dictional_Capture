6452db56fab16defcd7cdf63df4b06dbd1a4785b
Mitsuki Goto <minionnzu20041125@yahoo.co.jp>
2020-02-21 19:52:01 +0900

    create activities

--- app/src/main/java/com/mikn/dictional_caputure/localscan/LocalScanActivity.kt
+++ app/src/main/java/com/mikn/dictional_caputure/localscan/LocalScanActivity.kt
@@ -1,13 +1,13 @@
-package com.mikn.dictional_caputure
+package com.mikn.dictional_caputure.localscan
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.mikn.dictional_caputure.R
 
-class MainActivity : AppCompatActivity() {
+class LocalScanActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_local_scan)
     }
-    
 }
