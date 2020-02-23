46885db8182a2db4ddfbcb6a4d2b8d9718ab2108
Mitsuki Goto <minionnzu20041125@yahoo.co.jp>
2020-02-23 22:47:12 +0900

    View Transition

--- app/src/main/java/com/mikn/dictional_caputure/MainActivity.kt
+++ app/src/main/java/com/mikn/dictional_caputure/MainActivity.kt
@@ -1,13 +1,23 @@
 package com.mikn.dictional_caputure
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.mikn.dictional_caputure.capture.CaptureActivity
+import com.mikn.dictional_caputure.history.HistoryShowActivity
+import com.mikn.dictional_caputure.localscan.LocalScanActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        caputure.setOnClickListener{startActivity(Intent(applicationContext,CaptureActivity::class.java))}
+        localscan.setOnClickListener{startActivity(Intent(applicationContext,LocalScanActivity::class.java))}
+        history.setOnClickListener{startActivity(Intent(applicationContext,HistoryShowActivity::class.java))}
+
     }
     
 }
--- app/src/main/java/com/mikn/dictional_caputure/history/HistoryShowActivity.kt
+++ app/src/main/java/com/mikn/dictional_caputure/history/HistoryShowActivity.kt
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.mikn.dictional_caputure.R
 
-class HistoryActivity : AppCompatActivity() {
+class HistoryShowActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
